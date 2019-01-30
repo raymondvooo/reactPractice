a3659127f0f1883c059d57c547abf27e14b228ad
unknown <raymondvooo@gmail.com>
2019-01-30 11:57:52 -0800

    counter and reset

--- src/App.js
+++ src/App.js
@@ -2,26 +2,69 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+
+
+
+class Button extends React.Component {
+  handleClick = () => {
+    this.props.onClickFunction(this.props.incrementValue);
+  }
+  render() { 
+  return(
+		<button onClick={this.handleClick}>
+    +{this.props.incrementValue}
+    </button>
+    );	
+	 }
+  }
+  
+  class Reset extends React.Component {
+    reset = () => {
+    this.props.onClickFunction();
+      console.log("heyman");
+    }
+    render() {
+    return(
+    <button onClick={this.reset}>
+    Reset
+    </button>);
+    }
+  }
+  
+  const Result = (props) => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div>{props.counter}</div>);
+  }
+  
+
+class App extends Component {
+  state = {
+    counter: 0
+  };
+  
+  incrementCounter = (incrementValue) => {
+    this.setState((prevState) => ({
+    counter: prevState.counter + incrementValue
+   }));
+  }
+  
+  resetClick = () => {
+    this.setState(() => ({
+    counter: 0
+   }));
+  }
+  
+    render() {
+      return (
+        <div>
+        <Button incrementValue={1} onClickFunction = {this.incrementCounter} />
+        <Button incrementValue={2} onClickFunction = {this.incrementCounter} />
+        <Button incrementValue={3} onClickFunction = {this.incrementCounter} />
+        <Button incrementValue={4} onClickFunction = {this.incrementCounter} />
+        <Reset  onClickFunction = {this.resetClick}/>
+        <Result counter = {this.state.counter}/>
+        </div>
+        );
   }
 }
 
